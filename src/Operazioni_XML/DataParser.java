5f5898b1c185983f63abaf20b4822335b7124237
Abudo-S <abudo.mohamed123456789@gmail.com>
2021-01-27 20:51:41 +0100

    create_colourClass_SN-DataParser

--- src/Operazioni_XML/DataParser.java
+++ src/Operazioni_XML/DataParser.java
@@ -5,13 +5,53 @@
  */
 package Operazioni_XML;
 
+import java.util.ArrayList;
+import struttura_sn.*;
+
 /**
  *
  * @author dell
  */
 public class DataParser {
-    //the neutral colour should exist at index 0 of C in SN 
-    //predicate: [(]*([_a-zA-Z]+[_a-zA-Z0-9]*)\s*(<=|>=|<|>|=|!\s*=|\s+in\s+|\s*!\s*in\s+)\s*([_a-zA-Z]+[_a-zA-Z0-9]*)[)]*
-    //Guard: (!)?[(]*\s*[(]*predicate[)]*\s*[)]*(([&]{2}|[|]{2})[(]*\s*[(]*predicate[)]*\s*[)]*)*[)]*
     
+    private static SN sn;
+    
+    public DataParser(SN sn){
+        DataParser.sn = sn;
+    }
+    //predicate: [(]*([_a-zA-Z]+[_a-zA-Z0-9]*)\s*(<=|>=|<|>|=|!\s*=|\s+in\s+|\s*!\s*in\s+)\s*([_a-zA-Z]+[_a-zA-Z0-9]*)[)]*
+    //Guard: (!)?[(]*\s*[(]*predicate[)]*\s*[)]*(([&]{2}|[|]{2})[(]*\s*[(]*predicate[)]*\s*[)]*)*[)]* 
+     
+    public void add_colourClass(String class_name, String[] available_tokens){
+        sn.add_colourClass(new ColourClass(class_name, available_tokens));
+    }
+    
+    public void add_colourClass(String class_name, String[] available_tokens, String[] subclasses_names, ArrayList<String[]> subclasses_tokens){
+        ColourClass c = new ColourClass(class_name, available_tokens);
+        
+        for(var i = 0; i < subclasses_names.length; i++){
+            c.add_subclass(c.new SubColourClass(subclasses_names[i], subclasses_tokens.get(i)));
+            sn.add_colourClass(c);
+        }
+    }
+    
+    public void add_place(){
+       
+    }
+    
+    public void add_transition(){
+        
+    }
+    
+    public void connect_nodes_via_arcs(){
+        
+    }
+    
+    public void create_Marking(){
+        
+    }
+    
+    public SN get_sn(){
+        return sn;
+    }
 }
